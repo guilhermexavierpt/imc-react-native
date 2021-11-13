443c55b5a1358afeb65fcb2d619b523f26299857
Guilherme Xavier <guilhermexavierpt@hotmail.com>
2021-11-13 18:36:32 -0300

    Calcular IMC

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import styles from './styles/styles.js'
 
 export default function App() {
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+
+  function handleSubmit() {
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+    if (imc < 18.6) { alert('Você está abaixo do peso! ' + imc.toFixed(2)) }
+    else if (imc >= 18.6 && imc < 24.9) { alert('Peso ideal! ' + imc.toFixed(2)) }
+    else if (imc >= 24.9 && imc < 34.9) { alert('Levemente acima do peso! ' + imc.toFixed(2)) }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Calcule seu IMC</Text>
+      <TextInput
+        style={styles.input}
+        value={peso}
+        onChangeText={(peso) => setPeso(peso)}
+        placeholder="Peso (Kg)"
+        keyboardType="numeric"
+
+      />
+      <TextInput
+        style={styles.input}
+        value={altura}
+        onChangeText={(altura) => setAltura(altura)}
+        placeholder='Altura (Cm)'
+        keyboardType="numeric"
+
+      />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleSubmit}
+
+      >
+        <Text style={styles.buttonText}>Calcular</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles/styles.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#D3D3D3',
+
+  },
+  title: {
+    textAlign: 'center',
+    marginTop: 40,
+    fontSize: 30,
+    color: '#FFF'
+  },
+  input: {
+    backgroundColor: '#A9A9A9',
+    borderRadius: 10,
+    margin: 15,
+    padding: 10,
+    color: '#FFF',
+    fontSize: 23
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 15,
+    backgroundColor: '#41Aef4',
+    padding: 10
+  },
+
+  buttonText: {
+    fontSize: 25,
+    color: '#FFF',
+  }
+})
